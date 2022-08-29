0d82559f041d5c5e1b54548e0ee3c71f10299794
Xihan Fu <xihanfu@Xihans-MacBook-Pro.local>
2022-08-28 23:30:32 -0500

    add morgan

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const express = require('express')
 const app = express()
 
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandleMiddleware = require('./middleware/error-handler')
+
+//login package
+const morgan = require('morgan')
 
 //
 require('express-async-errors')
 // set dotenv
 require('dotenv').config()
+
+
 //set express.json()
 app.use(express.json())
 
+
+
 // set app
+app.use(morgan('tiny'))
 app.use('/',(req,res)=>{
     res.send('e-commerce')
 })
-
+// if not find the route, it will get into the middleware
 //set middleware
-const notFoundMiddleware = require('./middleware/not-found')
-const errorHandleMiddleware = require('./middleware/error-handler')
 app.use(notFoundMiddleware)
 app.use(errorHandleMiddleware)
 
