e0bf4eacbb0c766e0606bae17e6d53b11ff5a362
Xihan Fu <xihanfu@Xihans-MacBook-Pro.local>
2022-08-28 23:11:22 -0500

    set notFounfd and Handle errors middleware

--- app.js
+++ app.js
@@ -2,10 +2,36 @@ const express = require('express')
 const app = express()
 
 
+//
+require('express-async-errors')
+// set dotenv
+require('dotenv').config()
+//set express.json()
+app.use(express.json())
+
+// set app
+app.use('/',(req,res)=>{
+    res.send('e-commerce')
+})
+
+//set middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandleMiddleware = require('./middleware/error-handler')
+app.use(notFoundMiddleware)
+app.use(errorHandleMiddleware)
+
+
+
+// set database
+const connectDB = require('./db/connect')
+
+// set the port
 const port = process.env.port || 3000
 
+// listen the port
 const start= async ()=>{
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port,()=>{
             console.log(`it is listening the port ${port}`);
         })
@@ -14,4 +40,4 @@ const start= async ()=>{
         console.log(error);
     }
 }
-start()
+start() 
