183b3505933e91b017cda005d947887c52f7a9ab
Xihan Fu <xihanfu@Xihans-MacBook-Pro.local>
2022-08-28 21:01:24 -0500

    basic express server

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('E-Commerce API');
+const express = require('express')
+const app = express()
+
+
+const port = process.env.port || 3000
+
+const start= async ()=>{
+    try {
+        app.listen(port,()=>{
+            console.log(`it is listening the port ${port}`);
+        })
+    
+    } catch (error) {
+        console.log(error);
+    }
+}
+start()
